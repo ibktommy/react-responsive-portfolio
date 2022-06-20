34a8456b01287e355ade6de920e588b974a54341
ibktommy <atomquartz@gmail.com>
2022-06-20 21:33:34 +0100

    Initial Project Setup

---
+++ src/component/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header className='header'>
+        <div className='container d_flex'>
+          <div className='logo'>
+            <img src='' alt=''/>
+          </div>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
