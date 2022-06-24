fbcdec291d58681da187b11e7a4ffd0f9fdef805
ibktommy <atomquartz@gmail.com>
2022-06-24 22:29:46 +0100

    Installing react-typewrite package

--- src/component/Hero/Home.jsx
+++ src/component/Hero/Home.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { Typewriter } from "react-simple-typewriter";
+import "./Home";
+import hero from "../../pic/hero.png";
+import skill1 from "../../pic/skill1.png";
+import skill2 from "../../pic/skill2.png";
+import skill3 from "../../pic/skill3.png";
 
 const Home = () => {
 	return (
 		<>
-			<h1>Home Section</h1>
+			<section className="hero" id="home">
+				<div className="container f_flex top">
+					<div className="left top">
+						<h1>
+							Hi, I'm <span>Jone Lee</span>
+						</h1>
+						<h2>
+							a
+							<span>
+								<Typewriter
+									words={[" Professional Coder", " Developer"]}
+									loop
+									cursor
+									cursorStyle="|"
+									typeSpeed={90}
+									deleteSpeed={100}
+									delaySpeed={500}
+								/>
+							</span>
+						</h2>
+					</div>
+				</div>
+			</section>
 		</>
 	);
 };
