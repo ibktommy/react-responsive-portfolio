6a6b9b6b8de51eb9bb49d92c7cd803f4c867c60e
ibktommy <atomquartz@gmail.com>
2022-06-25 22:38:08 +0100

    Add image file and creating of Card Component for Potfolio Component

---
+++ src/component/Portfolio/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = ({  }) => {
+  return (
+    <>
+      <div className="box btn_shadow">
+        <div className="img">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card
--- src/component/Portfolio/Portfolio.jsx
+++ src/component/Portfolio/Portfolio.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Portfolio.css'
+import projects from './PortfolioData'
+import Card from "../Portfolio/Card";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <>
+      <section className="portfolio top" id="porfolio">
+        <div className="container">
+          <div className="heading text-center">
+            <h4>VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK</h4>
+            <h1>My Portfolio</h1>
+          </div>
+
+          <div className="content grid">
+            {
+              projects.map(({ id, ...props }) => {
+                return  <Card />
+              })
+            }
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
