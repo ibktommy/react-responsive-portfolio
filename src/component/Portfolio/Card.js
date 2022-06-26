a72b552fb2377b2284a32359e34bcb4a8db44c1f
ibktommy <atomquartz@gmail.com>
2022-06-26 12:45:57 +0100

    Adding the Modal in JSX

--- src/component/Portfolio/Card.js
+++ src/component/Portfolio/Card.js
@@ -31,6 +31,36 @@ const Card = ({ category, totalLike, title, image }) => {
           </a>
         </div>
       </div>
+
+      {/* POPUP MODAL */}
+      {modal && (
+        <div className="modal">
+          <div className="overlay">
+            <div className="modal-content d_flex">
+              <div className="modal-img left">
+                <img src={image} alt={title} />
+              </div>
+              <div className="modal-text right">
+                <span>Featured - Design</span>
+                <h1>{title}</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptas incidunt, sint nisi aspernatur assumenda?</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Numquam fuga maxime quo.</p>
+                <div className="button f_flex mtop">
+                  <button className="btn_shadow">
+                    LIKE THIS <i className='far fa-thumbs-up'></i>
+                  </button>
+                  <button className="btn_shadow">
+                    VIEW PROJECT <i className='fas fa-chevron-right'></i>
+                  </button>
+                </div>
+                <button className="close-modal btn">
+                  <i className="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   )
 }
