0c51e38f388562eacdc47c9d3e0eb4168138cf67
ibktommy <atomquartz@gmail.com>
2022-06-26 12:28:12 +0100

    Setting useState hook to manage the modal state

--- src/component/Portfolio/Card.js
+++ src/component/Portfolio/Card.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Portfolio.css'
 
 const Card = ({ category, totalLike, title, image }) => {
+  //Seting UseState Hook
+  const [modal, setModal] = useState(false)
+
+  // Function to Update "modal" state
+  function toggleModal() {
+    setModal(!modal)
+  }
+
   return (
     <>
       <div className="box btn_shadow">
         <div className="img">
-          <img src={image} alt={title} className="card-img" />
+          <img src={image} alt={title} className="card-img" onClick={toggleModal}/>
         </div>
         <div className="category d_flex">
-          <span>{category}</span>
+          <span>{category} onClick={toggleModal}</span>
           <label>
             <i className="far fa-heart">
               {totalLike}
@@ -17,7 +25,7 @@ const Card = ({ category, totalLike, title, image }) => {
           </label>
         </div>
         <div className="title">
-          <h2>{title}</h2>
+          <h2>{title} onClick={toggleModal}</h2>
           <a href="#popup" className='arrow'>
             <i className="fas fa-arrow-right"></i>
           </a>
